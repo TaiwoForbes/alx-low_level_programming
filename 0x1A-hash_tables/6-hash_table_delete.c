dcfcb6de29908200c2b84eeaf25ffbcb984f1726
root <soyemihissa2000@gmail.com>
2022-07-01 17:52:40 -0700

    corrections

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,61 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * free_list - Free a list.
- *
- * @head: Pointer to the head of a linked list
- *
- * Return: Void.
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hash table.
  */
-
-void free_list(hash_node_t *head)
-{
-	hash_node_t *placeholder_head = NULL;
-
-	if (!head)
-		return;
-
-	while (head)
-	{
-		placeholder_head = head;
-		if (head->key)
-			free(head->key);
-		if (head->value)
-			free(head->value);
-		head = head->next;
-		free(placeholder_head);
-	}
-}
-
-
-/**
- * hash_table_delete - Deletes a hash table
- *
- * @ht: Pointer to the hash table (hash_table_t *)
- *
- * Return: Void.
- */
-
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	hash_node_t **node = NULL;
-	hash_node_t *list = NULL;
+	hash_node_t *node;
 
-	if (!ht)
-		return;
-
-	if (!ht->array)
+	while (i < ht->size)
 	{
-		free(ht);
-		return;
+		while (ht->array[i] != NULL)
+		{
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
+		}
+		i++;
 	}
-
-	node = ht->array;
-
-	for (; i < ht->size; i++)
-		list = node[i], free_list(list);
-
 	free(ht->array);
 	free(ht);
 }
